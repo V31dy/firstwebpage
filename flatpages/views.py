3972745b02b0af5c7c6f5da34a47dec44ae2e473
V31dy <71170989+V31dy@users.noreply.github.com>
2024-07-06 16:48:06 +0300

    Update

---
+++ flatpages/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
+
+
+def home(request):
+    return render(request, 'templates/static_handler.html')
+
+
+def hello(request):
+    return HttpResponse('Hello, World!')
